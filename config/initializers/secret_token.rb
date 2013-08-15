2d14dadb4e9a4cdcf46df50c4275a37ce9387d47
takkyogawa <takkyogawa@gmail.com>
2013-08-15 22:44:14 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ib2test::Application.config.secret_token = '5053ec49aa4e1a79b5f7e61d4f7ec84d7fd96f737a148b91008fa020d04aca477988df9ed664a859c6fc8dd30fd85e7af23400295c19f989c63b7adf23d54155'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ib2test::Application.config.session_store :cookie_store, key: '_ib2test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ib2test::Application.config.session_store :active_record_store
